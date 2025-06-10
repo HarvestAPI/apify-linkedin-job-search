e1a20047febda7f5f0135a954009d1b56d00e427
xorcuit <xorcuit@gmail.com>
2025-06-11 01:01:04 +0700

    fix: allow input without job titles and location; add default location

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,6 @@ interface Input {
   geoIds?: string[];
   page?: number;
   maxItems?: number;
-
   company?: string[];
   sortBy?: string;
   workplaceType?: string[];
@@ -35,20 +34,10 @@ interface Input {
 const input = await Actor.getInput<Input>();
 if (!input) throw new Error('Input is missing!');
 input.jobTitles = (input.jobTitles || []).filter((q) => q && !!q.trim());
-if (!input.jobTitles?.length) {
-  console.error('Search queries: at least one Job Title is required.');
-  await Actor.exit();
-  process.exit(0);
-}
 input.locations = (input.locations || []).filter((l) => l && !!l.trim());
-if (!input.locations?.length) {
-  console.error('Locations Filter: at least one Location is required.');
-  await Actor.exit();
-  process.exit(0);
-}
 
 const { actorId, actorRunId, actorBuildId, userId, memoryMbytes } = Actor.getEnv();
-let { actorMaxPaidDatasetItems } = Actor.getEnv();
+const { actorMaxPaidDatasetItems } = Actor.getEnv();
 
 const scraper = createLinkedinScraper({
   apiKey: process.env.HARVESTAPI_TOKEN!,
@@ -62,12 +51,6 @@ const scraper = createLinkedinScraper({
     'x-apify-actor-max-paid-dataset-items': String(actorMaxPaidDatasetItems) || '0',
   },
 });
-
-if (!actorMaxPaidDatasetItems) {
-  actorMaxPaidDatasetItems = 10000000;
-}
-
-let maxItems = Number(input.maxItems) || actorMaxPaidDatasetItems || 10000000;
 
 const query = {
   company: input.company,
@@ -89,44 +72,58 @@ const locations: {
   ...(input.geoIds?.map((geoId) => ({ geoId })) || []),
 ];
 
+if (!locations.length) {
+  locations.push({ geoId: '92000000' }); // Default to global search if no locations provided
+}
+
 let lastPromise: Promise<any> | undefined;
 
-for (const searchQuery of input.jobTitles) {
-  let queryCounter = 0;
+const combinations: Record<string, string | string[] | undefined | boolean>[] = [];
 
-  for (const location of locations) {
-    if (actorMaxPaidDatasetItems && maxItems && maxItems > actorMaxPaidDatasetItems) {
-      maxItems = actorMaxPaidDatasetItems;
+for (const location of locations) {
+  if (input.jobTitles.length) {
+    for (const searchQuery of input.jobTitles) {
+      combinations.push({ search: searchQuery, ...query, ...location });
     }
-    let scrapedItems = 0;
-
-    if (maxItems - queryCounter <= 0) {
-      continue;
-    }
-
-    console.info(
-      `Scraping jobs for query "${searchQuery}" with location "${location.location || location.geoId}"...`,
-    );
-
-    await scraper.scrapeJobs({
-      query: {
-        ...(query as any),
-        search: searchQuery,
-        ...location,
-      },
-      outputType: 'callback',
-      onItemScraped: async ({ item }) => {
-        console.info(`Scraped job ${item.id}`);
-        scrapedItems++;
-        queryCounter++;
-        lastPromise = Actor.pushData(item);
-      },
-      overrideConcurrency: 6,
-      maxItems: maxItems - queryCounter,
-    });
-
-    actorMaxPaidDatasetItems -= scrapedItems;
+  } else {
+    combinations.push({ ...query, ...location });
   }
+}
+
+const state: {
+  itemsLeft: number;
+} = {
+  itemsLeft: actorMaxPaidDatasetItems || 1000000,
+};
+
+if (!combinations.length) {
+  console.error('No job titles or companies provided in the input. No data will be scraped.');
+}
+
+for (const combinationQuery of combinations) {
+  let maxItems = input.maxItems || state.itemsLeft;
+  if (maxItems > state.itemsLeft) {
+    maxItems = state.itemsLeft;
+  }
+
+  console.info(`Scraping jobs for query ${JSON.stringify(combinationQuery)}...`);
+
+  await scraper.scrapeJobs({
+    query: {
+      ...combinationQuery,
+    },
+    outputType: 'callback',
+    onItemScraped: async ({ item }) => {
+      console.info(`Scraped job ${item.id}`);
+      state.itemsLeft -= 1;
+
+      if (state.itemsLeft >= 0) {
+        lastPromise = Actor.pushData(item);
+      }
+    },
+    overrideConcurrency: 6,
+    maxItems,
+  });
 }
 
 if (lastPromise) {
